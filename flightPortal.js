5163f14f939c91b6e3ee8af73ea25f39f1017edf
mshibl <mohamed.shibl@hotmail.com>
2016-01-26 21:34:14 -0800

    adds HTTP package to the app for the purpose of making API requests

--- flightPortal.js
+++ flightPortal.js
@@ -1,4 +1,21 @@
 if (Meteor.isClient) {
+  HTTP.call( 'GET', 'http://jsonplaceholder.typicode.com/posts', {}, function( error, response ) {
+    if ( error ) {
+      console.log( error );
+    } else {
+      console.log( response );
+      /*
+       This will return the HTTP response object that looks something like this:
+       {
+         content: "String of content...",
+         data: Array[100], <-- Our actual data lives here. 
+         headers: {  Object containing HTTP response headers }
+         statusCode: 200
+       }
+      */
+    }
+  });
+
   // counter starts at 0
   Session.setDefault('counter', 0);
 
